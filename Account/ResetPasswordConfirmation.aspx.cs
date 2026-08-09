1eaa65b03bd7e1874c3040806b4d109f3ccbb93f
ShraddhaNeha06 <sukaishraddha12@gmail.com>
2026-08-09 14:50:25 +0200

    Initial project upload

---
+++ Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace UltimateDrip.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UltimateDrip.Global" Language="C#" %>
